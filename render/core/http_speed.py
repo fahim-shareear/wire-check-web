b56f539a1f6c9a852b059316fd4c195073bfc2d2
fahim-shareear <fahimshareear@gmail.com>
2026-05-09 00:39:03 +0600

    .......

--- render/core/http_speed.py
+++ render/core/http_speed.py
@@ -208,5 +208,10 @@ def health():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    port = int(os.environ.get("PORT", 10000))  # Render will override this
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
